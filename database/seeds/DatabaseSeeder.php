92f7e18861e261eb1dc212fcc6d65a1d8516b2a9
jamalbelilet <fd_belilet@esi.dz>
2019-02-07 10:21:04 +0100

    feat: user's seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,6 +6,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        $this->call(UsersTableSeeder::class);
         $this->call(TutorsTableSeeder::class);
         $this->call(CoursesTableSeeder::class);
         $this->call(StudentsTableSeeder::class);
