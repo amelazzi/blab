0aa28e3e1d70b2340eec911802bae261957f4dbf
Amel AZZI <ea_azzi@esi.dz>
2019-02-09 15:24:40 +0100

    fixing Tabs bug

--- public/js/app.js
+++ public/js/app.js
@@ -67874,37 +67874,72 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/es/index.js");
 /* harmony import */ var _styles_utilities_Colors__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../styles/utilities/Colors */ "./resources/js/src/styles/utilities/Colors.js");
-var _this = undefined;
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 
 
 
 
 
-var Tabs = function Tabs() {
-  state = {
-    activeTab: 1
-  };
-  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-    onClick: function onClick() {
-      return _this.setState({
-        activeTab: 1
-      });
+var Tabs =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(Tabs, _Component);
+
+  function Tabs() {
+    var _this;
+
+    _classCallCheck(this, Tabs);
+
+    _this.state = {
+      activeTabs: []
+    };
+    return _possibleConstructorReturn(_this);
+  }
+
+  _createClass(Tabs, [{
+    key: "render",
+    value: function render() {
+      var _this2 = this;
+
+      return react__WEBPACK_IMPORTED_MODULE_0__["React"].createElement(react__WEBPACK_IMPORTED_MODULE_0__["React"].Fragment, null, react__WEBPACK_IMPORTED_MODULE_0__["React"].createElement(react__WEBPACK_IMPORTED_MODULE_0__["React"].Fragment, null, react__WEBPACK_IMPORTED_MODULE_0__["React"].createElement("button", {
+        onClick: function onClick() {
+          return _this2.setState({
+            activeTab: 1
+          });
+        }
+      }, "tab1"), react__WEBPACK_IMPORTED_MODULE_0__["React"].createElement("button", {
+        onClick: function onClick() {
+          return _this2.setState({
+            activeTab: 2
+          });
+        }
+      }, "tab2"), react__WEBPACK_IMPORTED_MODULE_0__["React"].createElement("button", {
+        onClick: function onClick() {
+          return _this2.setState({
+            activeTab: 3
+          });
+        }
+      }, "tab3")), react__WEBPACK_IMPORTED_MODULE_0__["React"].createElement(react__WEBPACK_IMPORTED_MODULE_0__["React"].Fragment, null, this.state.activeTab == 1 ? react__WEBPACK_IMPORTED_MODULE_0__["React"].createElement("div", null, " tab 1 content ") : this.state.activeTab == 2 ? react__WEBPACK_IMPORTED_MODULE_0__["React"].createElement("div", null, " tab 2 content ") : react__WEBPACK_IMPORTED_MODULE_0__["React"].createElement("div", null, " tab 3 content ")));
     }
-  }, "tab1"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-    onClick: function onClick() {
-      return _this.setState({
-        activeTab: 2
-      });
-    }
-  }, "tab2"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-    onClick: function onClick() {
-      return _this.setState({
-        activeTab: 3
-      });
-    }
-  }, "tab3")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, state.activeTab == 1 ? react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, " tab 1 content ") : state.activeTab == 2 ? react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, " tab 2 content ") : react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, " tab 3 content ")));
-};
+  }]);
+
+  return Tabs;
+}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
 
 /* harmony default export */ __webpack_exports__["default"] = (Tabs);
 
--- resources/js/src/containers/Account/components/Tabs.js
+++ resources/js/src/containers/Account/components/Tabs.js
@@ -1,13 +1,16 @@
 
-import React from 'react';
+import { React, Component} from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import {purple, dark, lightGray} from '../../../styles/utilities/Colors'
 
-const Tabs = () => {
-    state = {
-        activeTab: 1
+class Tabs extends Component {
+    constructor() {
+        this.state = {
+          activeTabs: []
+        }
     }
+    render() {
         return (
             <>
                 <>
@@ -17,13 +20,13 @@ const Tabs = () => {
                 </>
                 <>
                     {
-                        state.activeTab == 1 ? <div> tab 1 content </div> : state.activeTab == 2 ?
+                        this.state.activeTab == 1 ? <div> tab 1 content </div> : this.state.activeTab == 2 ?
                         <div> tab 2 content </div> :
                         <div> tab 3 content </div>
                     }
                 </>
             </>
         );
-};
-
+    }
+}
 export default Tabs;
